51e1f8c7b7a16650d5f293cbfa276c83f91cbbd0
Crowdbotics <engineering@crowdbotics.com>
2023-03-23 01:42:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-wave-dev-71485",
+  url: "https://morning-wave-dev-71485.botics.co",
+  api: "https://morning-wave-dev-71485.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
